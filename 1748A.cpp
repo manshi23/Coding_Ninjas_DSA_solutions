42778ef20e171556102369155c3dd7c86f50568b
Manshi Malik <112588239+manshi23@users.noreply.github.com>
2023-01-06 21:29:31 +0530

    Create it

---
+++ 1748A.cpp
@@ -0,0 +1,14 @@
+//The Ultimate Square
+#include<bits/stdc++.h>
+#define ll long long
+using namespace std;
+int main(){
+    ll t;
+    cin>>t;
+    while(t--){
+        ll n;
+        cin>>n;
+        cout<<(n+1)/2<<endl;
+    }
+    return 0;
+}
